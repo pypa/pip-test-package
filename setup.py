54708ef265a2652de9adb1a27c60f07900362359
Richard Si <sichard26@gmail.com>
2025-07-18 16:37:54 -0400

    Add extra to project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pip-test-package',
-    version='0.1.1',
+    version='0.1.2',
     author='PyPA',
     author_email='pypa@pypa.pypa',
     url='https://github.com/pypa',
@@ -10,5 +10,8 @@ setup(name='pip-test-package',
     zip_safe=False,
     entry_points={
         'console_scripts': ['pip-test-package=piptestpackage:main'],
+    },
+    extras_require={
+        'extra': ["simple"]
     },
 )
