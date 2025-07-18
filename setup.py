96d6d72ac54132aecbdd5adac88bc8d1f8fb986b
Richard Si <sichard26@gmail.com>
2025-07-18 16:44:09 -0400

    Leave version alone, actually

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pip-test-package',
-    version='0.1.2',
+    version='0.1.1',
     author='PyPA',
     author_email='pypa@pypa.pypa',
     url='https://github.com/pypa',
